4540f2f6c7318fe7eaf8dc7466b0866201937458
Shinya Suzuki <shinya4250@yahoo.co.jp>
2017-04-09 12:17:26 +0900

    test

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,8 @@
 import {app} from "electron";
 import createWindow from "./createWindow";
+import setAppMenu from "./setAppMenu";
 app.on("ready",()=>{
+		setAppMenu();
 		createWindow();
 });
 app.on("window-all-closed",()=>{
--- src/renderer/app.jsx
+++ src/renderer/app.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 import {render} from "react-dom";
-render(<div>Hello, Electron and React JSX</div>,
-        document.getElementById("app"));
+import {Router, Route, hashHistory} from "react-router";
+import Login from "./Login";
+import Signup from "./Signup";
+import Rooms from "./Rooms";
+import Room from "./Room";
+
+// Routing difinition`
+const appRouting=(
+  <Router history={hashHistory}>
+    <Route path="/">
+      <Route path="login" component={Login} />
+      <Route path="singup" component={Signup} />
+      <Route path="rooms" component={Rooms}>
+        <Route path=":roomId" component={Room} />
+      </Route>
+    </Route>
+  </Router>
+);
+// Routing initialze
+if(!location.hash.length){
+  location.hash="#/login";
+}
+// Application drawing
+render(appRouting,document.getElementById("app"));
 
